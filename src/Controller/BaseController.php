ab388e713ecea2b47263927a15582e434a7e5f66
sefhirot69 <josalillo@gmail.com>
2023-03-04 14:46:41 +0100

    Feat: install serializer

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -9,11 +9,13 @@ use App\Shared\ExceptionsHttpStatusCodeMapping;
 use function Lambdish\Phunctional\each;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
 
 abstract class BaseController extends AbstractController
 {
     public function __construct(
         private readonly ExceptionsHttpStatusCodeMapping $exceptionMapping,
+        private readonly SerializerInterface $serializer,
     ) {
         each(
             fn (int $httpCode, string $exceptionClass) => $this->exceptionMapping->register($exceptionClass, $httpCode),
@@ -22,4 +24,9 @@ abstract class BaseController extends AbstractController
     }
 
     abstract protected function exceptions(): array;
+
+    protected function deserialize(string $content, string $class): mixed
+    {
+        return $this->serializer->deserialize($content, $class, 'json');
+    }
 }
