54206f4c06a066d724924e24894a0582619109f5
sefhirot69 <josalillo@gmail.com>
2023-03-15 19:53:41 +0100

    Feat: create EventDomain and add in AggregateRoot.php

--- Shared/Domain/Aggregate/AggregateRoot.php
+++ Shared/Domain/Aggregate/AggregateRoot.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace Shared\Domain\Aggregate;
 
+use Shared\Domain\Bus\Event\DomainEvent;
+
 class AggregateRoot
 {
+    private array $domainEvents = [];
+
+    final public function pullDomainEvents(): array
+    {
+        $domainEvents       = $this->domainEvents;
+        $this->domainEvents = [];
+
+        return $domainEvents;
+    }
+
+    final protected function record(DomainEvent $domainEvent): void
+    {
+        $this->domainEvents[] = $domainEvent;
+    }
 }
